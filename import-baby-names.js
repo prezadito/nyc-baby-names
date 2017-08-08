b1664a93fde60c035f358869e4dda673991759b9
Aditya Niloi <aditya4mvp@gmail.com>
2017-08-07 20:32:13 -0400

    initial commit

---
+++ import-baby-names.js
@@ -0,0 +1,13 @@
+// Access file system
+var fs = require('fs');
+
+// Pass it file to be read and then execute function
+fs.readFile('baby-names.csv', function(err, data) {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    var parsedData = data.toString('utf-8');
+
+    console.log(parsedData);
+})
